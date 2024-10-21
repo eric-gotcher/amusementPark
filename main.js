69d51f9f37e5fded8544880e73ea1f12140b9e9b
derekfrost12140 <34093825+derekfrost12140@users.noreply.github.com>
2024-10-21 17:47:00 -0500

    Firebase configuration for our app

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBn7xE-jaEuixzyDROnbHrQo6-YtOR5LaU",
+  authDomain: "amusement-park-4039d.firebaseapp.com",
+  projectId: "amusement-park-4039d",
+  storageBucket: "amusement-park-4039d.appspot.com",
+  messagingSenderId: "625618396056",
+  appId: "1:625618396056:web:ff2907be3a1958ed9041ed",
+  measurementId: "G-6QHBHT0PPE"
+};
