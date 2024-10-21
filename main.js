f0410f8e1aa0261c2e20543d34a41e7b5c4e68db
derekfrost12140 <34093825+derekfrost12140@users.noreply.github.com>
2024-10-21 17:46:13 -0500

    Importing the functions needed from the SDKs

---
+++ main.js
@@ -0,0 +1,4 @@
+// Importing the functions needed from the SDKs 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
